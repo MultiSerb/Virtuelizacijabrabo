9db22e4bc632c62032c342782053151bc63b633e
MultiSerb <121206533+MultiSerb@users.noreply.github.com>
2024-04-16 12:06:05 +0200

    Add files via upload

---
+++ Vezbe3/Zadatak1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberMenu = 0;
+            while (numberMenu != 2) 
+            {
+                numberMenu = printMainMenu();
+                if(numberMenu == 1)
+                {
+                    Console.WriteLine("Name of file: ");
+                    string path = Console.ReadLine();
+                    int functionNumber = 0;
+                    TextManipulation textManipulation = new TextManipulation(path);
+                    while (functionNumber != 5)
+                    {
+                        functionNumber = printTextManipulatingMenu();
+                        try
+                        {
+                            executeFunction(functionNumber, textManipulation);
+                        }
+                        catch (Exception e) {
+                            Console.WriteLine("ERROR : " + e.Message);
+                            break;
+                        }
+                    }
+                    textManipulation.Dispose();
+                }
+            } 
+        }
+
+        static int printMainMenu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("1. Manipulate files");
+            Console.WriteLine("2. End program");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int printTextManipulatingMenu()
+        {
+            Console.WriteLine("1. Add text");
+            Console.WriteLine("2. Read text");
+            Console.WriteLine("3. Delete text");
+            Console.WriteLine("4. Count words");
+            Console.WriteLine("5. Exit");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void executeFunction(int functionNumber, TextManipulation textManipulation)
+        {
+            switch (functionNumber)
+            {
+                case 1:
+                    Console.WriteLine("Unesite tekst");
+                    string text = Console.ReadLine();
+                    textManipulation.AddTextToFile(text);
+                    break;
+                case 2:
+                    Console.WriteLine("Tekst koji se nalazi u dokumentu");
+                    Console.WriteLine(textManipulation.ReadAllText());
+                    break;
+                case 3:
+                    textManipulation.DeleteAllText();
+                    break;
+                case 4:
+                    Console.WriteLine("Unesite reći koje želite da izbrojite odvojene zarezom");
+                    string[] words = Console.ReadLine().Replace(" ", "").Split(',');
+                    List<WordInformation> list = textManipulation.CountWordsInText(words);
+                    foreach (WordInformation word in list)
+                    {
+                        Console.WriteLine(word);
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Vezbe3/Zadatak1/TextManipulation.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak1
+{
+    struct WordInformation
+    {
+        public string word { get;  }
+        public int count{ get;  }
+        public WordInformation(string word,int count) 
+        {
+            this.word = word;
+            this.count = count;
+        }
+
+        public override string ToString()
+        {
+            return "Word : " + word + " Count : " + count;
+        }
+    }
+    class TextManipulation : IDisposable
+    {
+        private TextWriter textWriter;
+        private TextReader textReader;
+        private bool disposed = false;
+        string path = "";
+        public string Path { get => path; }
+
+        public TextManipulation(string path)
+        {
+            this.path = path;
+        }
+
+        ~TextManipulation()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (textWriter != null)
+                    {
+                        textWriter.Dispose();
+                    }
+                    if (textReader != null)
+                    {
+                        textReader.Dispose();
+                    }
+                }
+                disposed = true;
+            }
+        }
+
+        public void AddTextToFile(string text)
+        {
+            textWriter = File.AppendText(path);
+            textWriter.WriteLine(text);
+            textWriter.Close();
+        }
+
+        public string ReadAllText()
+        {
+            textReader = File.OpenText(path);
+            string text = textReader.ReadToEnd();
+            textReader.Close();
+            return text;
+        }
+
+        public List<WordInformation> CountWordsInText(params string[] args)
+        {
+            string[] allWords = ReadAllText().Replace("\r\n", " ").Replace("\r", " ").Split(' ');
+            List<WordInformation> listWords = new List<WordInformation>();
+
+            foreach (string word in args)
+            {
+                string[] allSameWords = allWords.Where(w => string.Compare(w,word, true) == 0).ToArray();
+                int number = allSameWords.Length;
+                listWords.Add(new WordInformation(word, number));
+            }
+            return listWords;
+        }       
+
+        public void DeleteAllText()
+        {
+            File.WriteAllText(path, string.Empty);            
+        }
+    }
+}
